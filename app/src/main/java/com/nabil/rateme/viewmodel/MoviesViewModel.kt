ff201f1493c8a3dda8c178d5cad857ba17f8af78
Nabil <ahmed.nabil@afaqy.com>
2019-06-22 06:46:34 +0200

    create viewModel

---
+++ app/src/main/java/com/nabil/rateme/viewmodel/MoviesViewModel.kt
@@ -0,0 +1,22 @@
+package com.nabil.rateme.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.nabil.rateme.model.Movie
+import com.nabil.rateme.model.Repository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class MoviesViewModel @Inject constructor(val moviesRepository: Repository) : ViewModel() {
+
+    fun insertMovies(vararg movie: Movie) {
+
+    }
+
+    fun loadMovies(vararg movie: Movie): Observable<List<Movie>> {
+        return Observable.fromIterable(emptyList())
+    }
+
+    fun updateMovie(name: String, rating: Int) {
+
+    }
+}
